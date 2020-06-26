ee8f3f61e8cc177e7912c055b156c2bf01792acd
Irene Scott <irenejinscott@gmail.com>
2020-06-26 04:27:36 +0000

    edited my logic in my plans controller

--- travelplanner/app/controllers/plans_controller.rb
+++ travelplanner/app/controllers/plans_controller.rb
@@ -1,32 +1,70 @@
 class PlansController < ApplicationController
 #create
     get "/plans" do
+      @plans = Plan.all
+      erb :'/plans/index'
+    end
+
+    get "/plans/new" do
+      @user = User.new
+      #@plan = Plan.find_by id:params["plan_id"]
+
       erb :'/plans/new'
     end
-#read
-    get "/plans/account" do
-      erb :'/plans/account'
-    end
-#update
-    get "/plans/:id/edit" do
-      @plans = Plan.find(params[:id])
-      erb :'/plans/edit'
+
+    post '/plans' do
+   @user = User.new
+   @plan = Plan.find_by id:params["plan_id"]
+
+    if !params["user"]["name"].empty?
+      @plan.user = User.create(name: params["user"]["name"])
     end
 
-    get "/plans/:id" do
-    @plan = Plan.find(params[:id])
-    erb :'/plans/account'
+    @plan.save
+
+    redirect to "plans/#{@plan.id}"
   end
 
-    patch "/plans/:id" do
-      @plan = Plan.find(params[:id])
-      @plan.update(params[:user][:plan])
-   redirect to "/plan/#{ @plan.id }"
+    #if !params["plan"]["plan_id"].empty?
+
+      #doing 2 diffferent actions in one route, you are creating and signing up a person
+      #but you are also creating a plan
+      #both of those created actions need to have checks to make sure they were filled in correctly
+      #suggested to move the following 3 lines into the plan class
+      #@user.plans << Plan.create(plan_id: params["plan"]["plan_id"],
+      #destination: params["destination"], mode_of_transport: params["mode_of_transport"],
+      #date: params["date"], user_id: @user.id)
+    #end
+#read
+    get "/plans/:id" do
+
+      @user = User.new
+      @plan = Plan.find_by id:params["plan_id"]
+
+     #@username = (params[:id][:username])
+      erb :'plans/new'
     end
+    #end
+#update
+  #  get "/plans/:id/edit" do
+  #    @plans = Plan.find(params[:id])
+  #    erb :'/plans/edit'
+  #  end
+
+  #  get "/plans/:id" do
+  #  @plan = Plan.find(params[:id])
+  #  erb :'/plans/account'
+#  end
+
+  #  patch "/plans/:id" do
+  #    @plan = Plan.find(params[:id])
+  #    @plan.update(params[:user][:plan])
+  # redirect to "/plan/#{ @plan.id }"
+  #  end
 #delete
-    delete "/plans/:id" do
-      Plan.destroy(params[:id])
-      redirect to "/plans/account"
-    end
+  #  delete "/plans/:id" do
+  #    Plan.destroy(params[:id])
+  #    redirect to "/plans/account"
+  #  end
 
 end
--- travelplanner/app/controllers/users_controller.rb
+++ travelplanner/app/controllers/users_controller.rb
@@ -1,91 +1,110 @@
 class UsersController < ApplicationController
 #new
     get "/signup" do
-     @plans = Plan.all
+     #@plans = Plan.all
      erb :'/users/signup'
     end
 
 
     post "/signup" do
-      @user = User.create(params[:user])
-      if !params["plan"]["plan_id"].empty?
-        #wanted to see if this format was correct in this line?
-        #doing 2 diffferent actions in one route, you are creating and signing up a person
-        #but you are also creating a plan
-        #both of those created actions need to have checks to make sure they were filled in correctly
-        #suggested to move the following 3 lines into the plan class
-        @user.plans << Plan.create(plan_id: params["plan"]["plan_id"],
-        destination: params["destination"], mode_of_transport: params["mode_of_transport"],
-        date: params["date"], user_id: @user.id)
-#if there is a user_id then thery are logged in
-#if@person!=nil && @person.password
-#  session[:owner_id] = @person.id
-# redirect to '/person/account'
-      end
-      redirect "/plans/new"
-    end
+      if params[:username] == "" || params[:password] == ""
+            redirect '/failure'
+          else
+            User.create(name: params[:name], username: params[:username], password: params[:password])
+             #binding.pry
+            redirect '/login'
+          end
+        end
+
+        #get '/account' do
+          #  @user = User.find(session[:user_id])
+        #    erb :'plans/account'
+          #end
+    #  @user = User.create(params[:user])
+      #if there is a user_id then thery are logged in
+    #if @user!=nil && @user.password
+    #  session[:user_id] = @user.id
+    #  redirect to '/account'
+    #end
+    #redirect "/account"
+    #end
 #read
     get "/login" do
 
-      @user = User.find(params[:id])
+     #@user = User.find(params[:id])
 
-      erb :'/users/login'
+     #@username = User.find(username: params[:id][:username])
+
+      erb :'/plans/account'
+
+    end
+
+    get '/users/:id' do
+      @user = User.find_by(id: params[:id])
+      if @user
+        erb :"/plans/account"
+#ask tomorrow about how to define instance variable for the username
+      else
+        redirect '/'
+      end
     end
 
     post "/login" do
-      @user = User.find(username: params[:id])
-      if @user && @user.authenticate(params[:password])
+     @user = User.create(username: params[:id])
+
+     @username = User.create(username: params[:id][:username])
+     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect to "/plans/account"
+     redirect to "/plans/account"
       else
       redirect to "/other/failure"
     end
     end
 
-    get "/users/account" do
-      @user = User.find(params[:id])
-      erb :'/plans/account'
-    end
+  #  get "/account" do
+  #    @user = User.find(params[:id])
+  #    erb :'/plans/account'
+  #  end
 #update
-    get "/users/:id/edit" do
-      @user = User.find(params[:id])
-      @plans = Plan.all
-      erb :'/users/edit'
-    end
+  #  get "/users/:id/edit" do
+  #    @user = User.find(params[:id])
+  #    @plans = Plan.all
+  #    erb :'/users/edit'
+  #  end
 
-    patch "/users/:id" do
-      @user = User.find(params[:id])
-      @user.update(params[:person])
+  #  patch "/users/:id" do
+  #    @user = User.find(params[:id])
+  #    @user.update(params[:person])
 
-      if !params["plan"]["plan_id"].empty?
-        @user.plans << Plan.create(plan_id: params["plan"]["plan_id"])
-      end
+  #    if !params["plan"]["plan_id"].empty?
+  #      @user.plans << Plan.create(plan_id: params["plan"]["plan_id"])
+  #    end
 
-      redirect "/users/#{@user.id}"
-    end
+  #    redirect "/users/#{@user.id}"
+  #  end
 
-    get "/other/failure" do
-        erb :failure
-      end
+  #  get "/other/failure" do
+    #    erb :failure
+    #  end
 
-    get "/logout" do
-      session.clear
-      redirect "/"
-    end
+  #  get "/logout" do
+  #    session.clear
+  #    redirect "/"
+  #  end
 #destroy
-    helpers do
-      def logged_in?
-        !!session[:user_id]
-      end
+    #helpers do
+    #  def logged_in?
+    #    !!session[:user_id]
+    #  end
 
-      def current_user
-        User.find(session[:user_id])
-      end
-    end
+    #  def current_user
+    #    User.find(session[:user_id])
+    #  end
 
-    delete "/users/:id" do
-      User.destroy(params[:id])
-      redirect to "/plans/account"
-    end
+
+  #  delete "/users/:id" do
+  #    User.destroy(params[:id])
+  #    redirect to "/plans/account"
+  #  end
 
 end
