61964530c014418d2e72142c5c540598895bf43b
codewithirene567 <irenejinscott@gmail.com>
2020-07-08 11:33:49 -0400

    erased comments and methods that were not needed

--- travelplanner/app/controllers/application_controller.rb
+++ travelplanner/app/controllers/application_controller.rb
@@ -7,36 +7,25 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, ENV['SECRET_PHRASE'] #switch back to this after local ENV['SESSION_SECRET']
-  end #this was call your password from the env file, so that users cannot see it
+  end
   
 
-  get '/' do #this line represents what happens when users navigate to the home page of the app
-    erb :'/users/index' #this line displays the users/index erb file
+  get '/' do
+    erb :'/users/index'
   end
 
-  helpers do #this is a helper method that defines what makes somebody logged in, in this case it is that the session number equals the user's id
+  helpers do 
     def logged_in?
       !!session[:user_id]
-      #ensure we are getting a truthy value, turns it into it's opposite truthiness
-      #turning it into a truthy value rather than it's actual value
-    end
-#NEW
-    def authorize_to_edit(plan) #accept an object as an argument, plans
-      #return true if the current user is the creator of the plan
-      #otherwise false
-      if !plan || plan.user != current_user
-        redirect to '/plans'
-      end
     end
 
-    def current_user #this defines what a current user is, it is a user which is set to an instance varaiable equal to whenever the right session is found
+    def current_user
       @user = User.find(session[:user_id])
     end
 
   end
 
-private #it is a way to designate that this method can be used locally
-#note: there is a lesson on it in ruby section
+private
 
   def redirect_if_logged_out(session) 
     if !logged_in?(session)
--- travelplanner/app/controllers/plans_controller.rb
+++ travelplanner/app/controllers/plans_controller.rb
@@ -1,50 +1,35 @@
 class PlansController < ApplicationController
-#create
-   get "/plans/new" do #this is the create action which represents what happens whenever somebody makes a new plan
-       if logged_in? #if they are logged in, then show them the erb plans/new page
+  
+   get "/plans/new" do
+       if logged_in?
         erb :'/plans/new'
       else
-        redirect to '/login' #otherwise make them login
+        redirect to '/login' 
       end
     end
 
-    post '/plans' do #creates a new travel plan and posts it so they can see it
-    #if you can't see it usually  wasn't created correctly
-    if params[:plan][:name] == "" || params[:plan][:destination] == "" || params[:plan][:mode_of_transport] == "" || params[:plan][:date] == ""
-      redirect to '/plans/new'
-    #this makes sure that the fields are not blank, otherwise it will take them to the new page to make the plan form be filled out correctly
-    else
-      #user = current_user
-   @plan = Plan.create(name: params[:plan][:name],
-   destination: params[:plan][:destination], mode_of_transport: params[:plan][:mode_of_transport],
-   date: params[:plan][:date], user_id: current_user.id)
-#this gives each of the plans their attributes
-      #@plan.user = current_user
-
-    #@plan.save! #if for some reason the plan didn't save, then raise an error
-    redirect to "/plans" #taking us to a specific plan
-    #redirect to "/plans/#{@plan.id}"
-    end
-  end
-
-#read
+    post '/plans' do 
+        if params[:plan][:name] == "" || params[:plan][:destination] == "" || params[:plan][:mode_of_transport] == "" || params[:plan][:date] == ""
+          redirect to '/plans/new'
+        else
+          @plan = Plan.create(name: params[:plan][:name],
+          destination: params[:plan][:destination], mode_of_transport: params[:plan][:mode_of_transport],
+          date: params[:plan][:date], user_id: current_user.id)
+            redirect to "/plans"
+        end
+      end
 
     get "/plans" do
       if logged_in?
          @user = current_user
          @plans = @user.plans
-        #erb :'/plans/index'
-        #binding.pry
           erb :'users/show'
-          #erb :'/plans/show'
       else
         redirect to '/login'
       end
     end
 
     get "/plans/search" do
-      #load a view that will display the search results
-      #redirect_if_logged_out(session)
       if !logged_in?
         redirect to '/login'
       end
@@ -55,32 +40,18 @@ class PlansController < ApplicationController
       erb :'/plans/search'
     end
 
-    #get "/plans/:id" do
-    #  if logged_in? #typically in get request params come from url and posts have it come from the form
-      #    @plan = Plan.find_by_id(params[:id])#the only thing that can get sent into find is an id
-      #    if @plan.user_id == session[:user_id]
-      #    erb :'/plans/show'
-      #    elsif @plan.user_id != session[:user_id]
-      #      redirect '/plans'
-      #    end
-      #  else
-      #    redirect to '/plans'
-      #  end
-      #end
-
      get "/plans/:id" do
-        if !logged_in?  #these three lines could be turned into a helper method
+        if !logged_in? 
           redirect to '/login'
         end
-        @plan = Plan.find_by_id(params[:id])#the only thing that can get sent into find is an id
+        @plan = Plan.find_by_id(params[:id])
         if @plan.user_id == session[:user_id]
         erb :'/plans/show'
         elsif @plan.user_id != session[:user_id]
           redirect '/plans'
         end
       end
-    #end
-#update
+    
     get "/plans/:id/edit" do
       if logged_in?
         @plan = Plan.find_by_id(params[:id])
@@ -98,14 +69,12 @@ class PlansController < ApplicationController
       if params[:plan][:name] == "" || params[:plan][:destination] == "" || params[:plan][:mode_of_transport] == "" || params[:plan][:date] == ""
         redirect to "/plans/#{params[:id]}/edit"
       else
-      @plan = current_user.plans.find_by_id(params[:id])#found the plan be looking at the current user's plans
+      @plan = current_user.plans.find_by_id(params[:id])
       @plan.name = params[:plan][:name]
       @plan.destination = params[:plan][:destination]
       @plan.mode_of_transport = params[:plan][:mode_of_transport]
       @plan.date = params[:plan][:date]
-      #@plan.user_id = current_user.id
       @plan.save
-      #@plan.update(params[:user][:plan])
       redirect to "/plans/#{@plan.id}"
       end
      end
@@ -115,10 +84,10 @@ class PlansController < ApplicationController
         erb :'/failure'
       end
 
-    #delete '/plans/delete' do
+   
     delete '/plans/:id' do
       if logged_in?
-         @plan = current_user.plans.find_by(params[:id]) #find the plan and then if that plan has a user id which is equal to the session id then take it back to plans
+         @plan = current_user.plans.find_by(params[:id])
          @plan.destroy
          redirect to "/plans"
      else
@@ -127,8 +96,4 @@ class PlansController < ApplicationController
     end
 
   end
-      #redirect '/users/show'
-
-      #Plan.destroy(params[:id])
-      #redirect to "/users/show"
-    #end
+     
