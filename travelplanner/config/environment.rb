3cfd78eca5171118311025c621a17fee43234df3
codewithirene567 <irenejinscott@gmail.com>
2020-10-29 23:24:23 -0400

    prep for heroku deployment

--- travelplanner/config/environment.rb
+++ travelplanner/config/environment.rb
@@ -1,13 +1,16 @@
 ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler'
-Bundler.require
-require 'dotenv/load'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+# require 'dotenv/load'
+Dotenv.load if ENV['SINATRA_ENV'] == "development"
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+# )
+
+set :database_file, "./database.yml"
 
 require './app/controllers/application_controller.rb'
 require_all 'app'
